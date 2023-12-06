18659d4973e44b83f36b730f56086c6812b1bd86
NicolasCareca <nicolas10morschel@gmail.com>
2023-12-06 16:50:56 -0300

    Revert "docker"
    This reverts commit c6691d0ccf408cd6a5483127a09c0ebd4abdc61c.

--- config/application.rb
+++ config/application.rb
@@ -2,41 +2,26 @@ require_relative "boot"
 
 require "rails/all"
 
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Mec
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 7.1
 
-    # Load the secret key base from credentials
-    config.secret_key_base = Rails.application.credentials.secret_key_base
-
-    # Configure the default encoding used in templates for Ruby 3.0
-    config.encoding = "utf-8"
-
-    # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
-
-    # Enable the asset pipeline
-    config.assets.enabled = true
-
-    # Other configurations...
-
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
 
-    # Store uploaded files on the local file system (see config/storage.yml for options)
-    config.active_storage.service = :local
-
-    # Time zone
-    config.time_zone = "UTC"
-    config.active_record.default_timezone = :utc
-
-    # Use the queuing backend for Active Job (default is :async)
-    config.active_job.queue_adapter = :sidekiq
-
-    # Middleware configurations...
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
-s
